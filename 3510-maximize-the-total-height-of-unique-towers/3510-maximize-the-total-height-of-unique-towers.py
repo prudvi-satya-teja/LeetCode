c7a80ac2c21698ce487722436736acc3a896b604
Prudvi Satya Teja Bachala <142888267+prudvi-satya-teja@users.noreply.github.com>
2024-10-01 11:17:19 +0530

    Time: 1010 ms (53.71%), Space: 31.4 MB (46.5%) - LeetHub

--- 3510-maximize-the-total-height-of-unique-towers/3510-maximize-the-total-height-of-unique-towers.py
+++ 3510-maximize-the-total-height-of-unique-towers/3510-maximize-the-total-height-of-unique-towers.py
@@ -1,10 +1,10 @@
 class Solution:
     def maximumTotalSum(self, maximumHeight: List[int]) -> int:
         maximumHeight.sort(reverse=True)
-        for i in range(len(maximumHeight)-1):
-            if maximumHeight[i] <= maximumHeight[i+1]:
-                maximumHeight[i+1]=maximumHeight[i]-1
-            if maximumHeight[i+1]==0:
+        for i in range(1,len(maximumHeight)):
+            if maximumHeight[i-1] <= maximumHeight[i]:
+                maximumHeight[i]=maximumHeight[i-1]-1
+            if maximumHeight[i]==0:
                 return -1
         return sum(maximumHeight)
         
