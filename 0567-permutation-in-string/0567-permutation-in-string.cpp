950c0e47ccba0d7a095c323b031b45ca4ce2a9c3
Prudvi Satya Teja Bachala <142888267+prudvi-satya-teja@users.noreply.github.com>
2024-10-05 06:50:23 +0530

    Time: 3 ms (94.61%), Space: 8.8 MB (80.36%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,11 +3,30 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.size();
         int m= s2.size();
-        sort(s1.begin(), s1.end());
-        for(int i=0; i<=m-n; i++) {
-            string sub = s2.substr(i, i + n-i);
-            sort(sub.begin(), sub.end());
-            if(s1 == sub)  return true;
+        
+        vector<int> v(26, 0);
+        for(int i=0; i<n; i++) {
+            int  ch = s1[i] - 'a';
+            v[ch]++;
+        }
+
+        for(int i=0; i<m; i++) {
+            int ch = s2[i] - 'a';
+            if(i < n-1) {
+                v[ch]--;
+            }
+            else {
+                v[ch]--;
+                for(int j=0; j<26; j++) {
+                    if(v[j] == 0 && j == 25) {
+                        return true;
+                    }
+                    else if(v[j] != 0) break;
+                }
+
+                int prev = s2[i-n+1] - 'a';
+                v[prev]++;
+            }
         }
         return false;
         
